1dd16c16df3616e0afd13f841b8c8e02c1d7e7a0
MuzafferYenice <muzafferyenicet@gmail.com>
2022-05-12 13:44:37 +0300

    12/05

--- src/lambdaPracticeElifHoca/C5_Method_Referance_String.java
+++ src/lambdaPracticeElifHoca/C5_Method_Referance_String.java
@@ -8,8 +8,14 @@ import java.util.stream.Collectors;
 
 public class C5_Method_Referance_String {
     public static void main(String[] args) {
-        List<String> l = Arrays.asList("Elma", "Muz", "Portakal", "Cilek", "Limon");
+        List<String> l = Arrays.asList("Elma", "Muz", "Portakal", "Cilek", "Limon","erik");
         buyukHarfliBosluklu(l);
+        System.out.println("***********");
+        uzunlugaGoreSirala(l);
+        System.out.println("***********");
+        eIleBaslayanalariAL(l);
+
+
     }
     // S1: Tum harfleri buyuk harf ile aralarinda bosluk birakarak yazdiralim
     public static void buyukHarfliBosluklu(List<String> l) {
@@ -21,10 +27,18 @@ public class C5_Method_Referance_String {
                 collect(Collectors.toList()));
     }
 
-    //S2: Stringleri uzunluklarina gore siralayip yaziniz
-
+    //S2: Stringleri uzunluklarina gore kucukten  buyuge siralayip yaziniz
+    public static void uzunlugaGoreSirala(List<String> l) {
+        l.stream().sorted(Comparator.comparing(String::length)).forEach(t-> System.out.print(t+" "));
+        System.out.println();
+        l.stream().sorted(Comparator.comparing(String::length).reversed()).forEach(t-> System.out.print(t+" "));
+    }
 
     //S3: E ILE Baslayanlari yazdiralim
+    public static void eIleBaslayanalariAL(List<String> l) {
+        System.out.println(l.stream().filter(t -> t.startsWith("E")||t.startsWith("e")).collect(Collectors.toList()));
+        System.out.println();
+     l.stream().map(String::toUpperCase).filter(t -> t.startsWith("E")).forEach(t-> System.out.print(t+" "));
 
-
+    }
 }
--- src/lambdaPracticeElifHoca/C6_Lambdada_ForLoop_Aralik_Kontrolu.java
+++ src/lambdaPracticeElifHoca/C6_Lambdada_ForLoop_Aralik_Kontrolu.java
@@ -1,48 +1,105 @@
 package lambdaPracticeElifHoca;
 
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class C6_Lambdada_ForLoop_Aralik_Kontrolu {
 
     public static void main(String[] args) {
+        birden30olanSayilariSirala();
+        System.out.println("\n" + "\n*********");
+        birden30olanSayilariSirala2();
+        System.out.println("\n*********");
+        ikiDegerTopla();
+        System.out.println("\n*********");
+        ortBul3040();
+        System.out.println("\n*********");
+        bolunen8e();
+        System.out.println("\n*********");
+        bolunen8eYaz();
+        System.out.println("\n*********");
+        bolunen8eTopla();
+        System.out.println("\n*********");
+        carp7ile15();
+        System.out.println("\n*********");
+        pozitifTekIlk10AL();
+        System.out.println("\n*********");
+        tekYaz21DenBuyukOlan();
+        System.out.println("\n*********");
+        tekYaz21DenBuyukOlanTopla();
+        System.out.println("\n*********");
 
 
     }
 
     // S1:1 den 30 kadar olan sayilari (30 dahil degil) 1 2 3 .... seklinde siralayalim (for loopsuz)
     //range(int startInclusive, int endExclusive)
-
+    public static void birden30olanSayilariSirala() {
+        IntStream.iterate(1, t -> t + 1).limit(30).forEach(t -> System.out.print(t + " "));
+        System.out.println();
+        IntStream.range(1, 31).forEach(t -> System.out.print(t + " "));
+    }
 
     //S2 1 den 30 kadar olan sayilari (30 dahil ) 1 2 3 .... seklinde siralayalim (for loopsuz)
     //rangeClosed(int startInclusive, int endInclusive)
+    public static void birden30olanSayilariSirala2() {
+        IntStream.rangeClosed(1, 30).forEach(t -> System.out.print(t + " "));
+    }
 
+    //S3 Istenen iki deger(dahil arasindaki sayilari toplayalim
+    public static void ikiDegerTopla() {
+        System.out.println(IntStream.rangeClosed(1, 30).sum());
+    }
 
-    //S3 Istenen iki deger(dahi) arasindaki sayilari toplayalim
-
-
-    //S4: 30 ile 40 arasindaki sayilarin (dahi) ortalamasini bulalim
-
+    //S4: 30 ile 40 arasindaki sayilarin (dahil) ortalamasini bulalim
+    public static void ortBul3040() {
+        System.out.println(IntStream.rangeClosed(30, 40).average());
+    }
 
     //S5: 325 ile 468 arasinda 8 e bolunen kac sayi vardir?
+    public static void bolunen8e() {
+        System.out.println(IntStream.rangeClosed(325, 468).filter(t -> t % 8 == 0).count());
+    }
 
     //S6: 325 ile 468 arasinda 8 bolunen sayilari yazdiralim
-
+    public static void bolunen8eYaz() {
+        IntStream.rangeClosed(325, 468).filter(t -> t % 8 == 0).forEach(t -> System.out.print(t + " "));
+    }
 
     // S7:325 ile 468 arasinda 8 bolunen sayilarin toplamini bulalim
-
+    public static void bolunen8eTopla() {
+        System.out.println(IntStream.rangeClosed(325, 468).filter(t -> t % 8 == 0).sum());
+    }
 
     // S8: 7ile 15 arasindaki tek sayilarin carpimini bulalim
+    public static void carp7ile15() {
+        IntStream.rangeClosed(7, 15).filter(t -> t % 2 == 1).map(t -> t * t).forEach(t -> System.out.print(t + " "));
+        System.out.println();
+        System.out.println(IntStream.iterate(7, t -> t + 2).limit(5).reduce(1, (m, y) -> m * y));
+        System.out.println();
+        System.out.println(IntStream.rangeClosed(7, 15).filter(t -> t%2==1).reduce(1, (m, y) -> m * y));
 
-
+    }
     //S9: pozitif tek sayilarin ilk 10 elemanin yazdiralim
+    public static void pozitifTekIlk10AL() {
+        IntStream.rangeClosed(1,200).filter(t -> t%2==1).limit(10).forEach(t -> System.out.print(t + " "));
+        System.out.println();
+        IntStream.iterate(1,t->t+2).limit(10).forEach(t -> System.out.print(t + " "));//coook basarili cleaner
 
+    }
 
     //S10: 21 den baslayan 7 nin katlarinin tek olanlari ilk 10 teriminin yaziralim
+    public static void tekYaz21DenBuyukOlan() {
+        IntStream.iterate(21,t->t+7).filter(t->t%2==1).limit(10).forEach(t -> System.out.print(t + " "));
 
-
+    }
 
     //S11: 21 den baslayan 7 nin katlarinin ilk 20 teriminin toplayalim
-
+    public static void tekYaz21DenBuyukOlanTopla() {
+        System.out.println(IntStream.iterate(21, t -> t + 7).limit(20).sum());
+    }
 }
 //iterate bize sayilari istedigimiz sekilde yineletmeyi saglar yani buradaki ornekte 7ser7ser artmayi saglar
 //iterate icin bir baslangic degeri girilmeli ve artisin nasil olacagi belirtilmeli nerede biteceginide
